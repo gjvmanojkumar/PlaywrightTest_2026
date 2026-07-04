b2c339c72fd24518cb4b62712414ea724109c683
ManojG <mk.guggilam@gmail.com>
2026-07-04 08:46:56 +0530

    update report type for CICD

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,9 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 3 : undefined,
   failOnFlakyTests: true,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html'], ['line'], ['allure-playwright']
-  ],
+  reporter: process.env.CI? 'blob' : [['html'], ['line'], ['allure-playwright']],
   timeout: 30000,  //30secs default timeout
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
