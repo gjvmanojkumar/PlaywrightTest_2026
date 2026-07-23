95b8dce1a844b64058996e97c9cfa733ef612bb8
ManojG <mk.guggilam@gmail.com>
2026-07-23 22:05:56 +0530

    Set fullyparallel to False

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,13 +14,13 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   failOnFlakyTests: true,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI? [['blob', { outputDir: 'blob-report' }], ['list']] : [['html', {open: 'on-failure'}], ['line']],
